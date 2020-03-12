88286b49cdeeda2786bc7572af5988adac5c979a
WarrenMfg <warrenmfg@icloud.com>
2020-03-12 10:17:10 -0400

    changed route names; added path.resolve to express.static

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const router = require('./routes.js');
+const path = require('path');
 
-app.use(express.static('./client/dist'));
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use('/', router);
+app.use('/api', router);
 
 module.exports.app = app;
 app.listen(process.env.PORT || 8080, () => console.log('Listening on port 8080!'));
