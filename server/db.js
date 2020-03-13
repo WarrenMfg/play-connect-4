2662da56ced783d5e9221b5e37db5df3313cc2e9
WarrenMfg <warrenmfg@icloud.com>
2020-03-12 21:32:33 -0400

    changed database from connectFour to heroku_d6c878d67fe5ca4

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: 'connectFour'
+  database: 'heroku_d6c878d67fe5ca4'
 });
 
 db.connect();
